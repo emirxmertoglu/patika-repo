6e62afe4e607ea47f7714164a6e2ff8db826c190
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-11-23 23:16:52 +0300

    🚧 Book api get methods created

---
+++ DotnetCore/BookStore/WebApi/Controllers/BookController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+
+    public class BookController : ControllerBase
+    {
+        public static List<Book> BookList = new List<Book>()
+        {
+            new Book{
+                Id = 1,
+                Title = "Lean Startup",
+                GenreId = 1, // Personal Growth
+                PageCount = 200,
+                PublishDate = new DateTime(2001, 06, 12)
+            },
+            new Book{
+                Id = 2,
+                Title = "Herland",
+                GenreId = 2, // Science Fiction
+                PageCount = 250,
+                PublishDate = new DateTime(2010, 05, 23)
+            },
+            new Book{
+                Id = 3,
+                Title = "Dune",
+                GenreId = 2, // Personal Growth
+                PageCount = 540,
+                PublishDate = new DateTime(2001, 12, 21)
+            }
+        };
+
+        // There must be only one HttpGet method without parameters.
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(book => book.Id).ToList<Book>();
+            return bookList;
+        }
+
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(book => book.Id == id).SingleOrDefault();
+            return book;
+        }
+
+        // [HttpGet]
+        // public Book GetBook([FromQuery] string id)
+        // {
+        //     var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
+        //     return book;
+        // }
+    }
+}
