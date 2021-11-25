36fa243a9120d0ef9745ec807210a82a205d0336
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-11-26 02:37:37 +0300

    ♻️ coded delete endpoint

--- DotnetCore/BookStore/WebApi/Controllers/BookController.cs
+++ DotnetCore/BookStore/WebApi/Controllers/BookController.cs
@@ -84,5 +84,18 @@ namespace WebApi.Controllers
 
             return Ok();
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+
+            if (book is null)
+                return BadRequest();
+
+            BookList.Remove(book);
+
+            return Ok();
+        }
     }
 }
