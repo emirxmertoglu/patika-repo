980f27457ac77a28850a195c70998bb434c65e80
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-11-23 23:01:43 +0300

    initial: c# variables and data types project

---
+++ CSharp-101/variables-and-data-types/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace variables_and_data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // I'm not a new developer, so I just want to try some interesting things
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            string todayFull = DateTime.Now.ToString("F");
+            Console.WriteLine(todayFull);
+            string todayShort = DateTime.Now.ToString("dd MMM yyyy ddd");
+            Console.WriteLine(todayShort);
+            string todayShorter = DateTime.Now.ToString("d");
+            Console.WriteLine(todayShorter);
+        }
+    }
+}
