5512fdb6762273e70cdb31b466ef5b17c92624be
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-11-24 00:11:52 +0300

    initial: type conversions

---
+++ CSharp-101/type-conversions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace type_conversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Implicit Conversions ****");
+            float a; int b = 25; a = b;
+            Console.WriteLine("a = {0}", a);
+
+            Console.WriteLine("**** Explicit Conversions ****");
+            int c = 25; float d; d = (float)c;
+            Console.WriteLine("d = {0}", d);
+            short x = 10; int y; y = x;
+            Console.WriteLine("y = {0}", y);
+            double m; float n = 10.5f; m = (double)n;
+            Console.WriteLine("m = {0}", m);
+        }
+    }
+}
