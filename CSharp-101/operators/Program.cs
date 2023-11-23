2f1001f61843ddf9a6932de5d882e35b91305897
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-11-23 23:16:22 +0300

    initial: c# operators project

---
+++ CSharp-101/operators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isHigher = 10 > 5;
+            Console.WriteLine(isHigher);
+            bool isLower = 10 < 5;
+            Console.WriteLine(isLower);
+            bool isEqual = 10 == 5;
+            Console.WriteLine(isEqual);
+            bool isNotEqual = 10 != 5;
+            Console.WriteLine(isNotEqual);
+            bool isHigherOrEqual = 10 >= 5;
+            Console.WriteLine(isHigherOrEqual);
+            bool isLowerOrEqual = 10 <= 5;
+            Console.WriteLine(isLowerOrEqual);
+
+            if (isHigher || isHigherOrEqual)
+            {
+                Console.WriteLine("is higher or is equal");
+            }
+
+            if (!isLower && !isLowerOrEqual)
+            {
+                Console.WriteLine("is lower and is lower or equal");
+            }
+
+            if (!isEqual)
+            {
+                Console.WriteLine("is not equal");
+            }
+        }
+    }
+}
