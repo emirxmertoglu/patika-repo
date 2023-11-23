b4c89c735498722c2d87beeaa752377b5564baba
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-11-24 00:38:20 +0300

    init: error handling

---
+++ CSharp-101/error-handling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace error_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = Int32.Parse(Console.ReadLine());
+                int b = Int32.Parse(Console.ReadLine());
+                int c = a + b;
+                Console.WriteLine("Sum = {0}", c);
+            }
+            catch (ArgumentNullException er)
+            {
+                Console.WriteLine(er.Message);
+            }
+            catch (FormatException er)
+            {
+                Console.WriteLine(er.Message);
+            }
+            catch (OverflowException er)
+            {
+                Console.WriteLine(er.Message);
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine(er.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Process Completed");
+            }
+        }
+    }
+}
