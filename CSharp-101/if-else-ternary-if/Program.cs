db85ef2df5d01752d3c92d6ba625ba0836c03bca
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-11-25 22:54:10 +0300

    add: if, else if, else, and ternary if

---
+++ CSharp-101/if-else-ternary-if/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace if_else_ternary_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int time = DateTime.Now.Hour;
+            int time = 8;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Good morning.");
+            }
+            else if (time >= 11 && time < 18)
+            {
+                Console.WriteLine("Good afternoon.");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+
+
+            string result =
+                time >= 6 && time < 11 ? "Good morning."
+                : time >= 11 && time < 18 ? "Good afternoon."
+                : time <= 18 ? "Good day."
+                : "Good evening.";
+            Console.WriteLine(result);
+
+        }
+    }
+}
