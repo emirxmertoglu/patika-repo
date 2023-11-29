7c993b89830020489a053c41ba5855275a72013b
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-11-29 21:35:18 +0300

    initial: method overloading

---
+++ CSharp-101/method-overloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace method_overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20, c = 30;
+            Console.WriteLine(Sum(a, b));
+            Console.WriteLine(Sum(a, b, c));
+            Console.WriteLine(Sum(a, b, out int sum));
+            Console.WriteLine(sum);
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static int Sum(int a, int b, out int sum)
+        {
+            sum = a + b;
+            return sum;
+        }
+    }
+}
