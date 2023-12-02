c1390f3348dd7a614f9eea110a9765b9e05b87d6
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-12-03 00:29:35 +0300

    init: string methods

---
+++ CSharp-101/string-methods/Program.cs
@@ -0,0 +1,52 @@
+﻿string str = "Our lesson is CSharp, Welcome!";
+string str2 = "Emir";
+
+// Length
+Console.WriteLine(str.Length);
+
+// ToUpper, ToLower
+Console.WriteLine(str.ToUpper());
+Console.WriteLine(str.ToLower());
+
+// Concat
+Console.WriteLine(string.Concat(str, " ", str2));
+
+// Compare, CompareTo
+Console.WriteLine(str.CompareTo(str2));
+Console.WriteLine(String.Compare(str, str2, true));
+Console.WriteLine(String.Compare(str, str2, false));
+
+// Contains
+Console.WriteLine(str.Contains("CSharp"));
+Console.WriteLine(str.StartsWith("Our"));
+Console.WriteLine(str.EndsWith("Welcome Buddy!"));
+
+// IndexOf
+Console.WriteLine(str.IndexOf("CSharp"));
+Console.WriteLine(str.IndexOf("Emir"));
+Console.WriteLine(str.LastIndexOf("e"));
+
+// Insert
+Console.WriteLine(str.Insert(0, "Hello! "));
+
+// PadLeft, PadRight
+Console.WriteLine(str + str2.PadLeft(30));
+Console.WriteLine(str + str2.PadLeft(30, '*'));
+Console.WriteLine(str.PadRight(50) + str2.PadLeft(30, '-'));
+Console.WriteLine(str.PadRight(50, '-') + str2.PadLeft(30, '#'));
+
+// Remove
+Console.WriteLine(str.Remove(15));
+Console.WriteLine(str.Remove(5, 3));
+Console.WriteLine(str.Remove(0, 1));
+
+// Replace
+Console.WriteLine(str.Replace("CSharp", "C#"));
+Console.WriteLine(str.Replace(" ", "*"));
+
+// Split
+Console.WriteLine(str.Split(' ')[3]);
+
+// Substring
+Console.WriteLine(str.Substring(1));
+Console.WriteLine(str.Substring(1, 6));
