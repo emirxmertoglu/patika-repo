c3469a0ee67a5069512629cc9edb9a1c44db3314
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-12-06 23:01:15 +0300

    list methods

--- CSharp-101/generic-collections/Program.cs
+++ CSharp-101/generic-collections/Program.cs
@@ -38,3 +38,36 @@ Console.WriteLine("###########");
 
 // Contains
 Console.WriteLine($"Is 11 in numberList: {numberList.Contains(11)}");
+
+// BinarySearch
+Console.WriteLine("###########");
+Console.WriteLine($"Index of 45 in numberList: {numberList.BinarySearch(45)}");
+Console.WriteLine("###########");
+
+// Array to List
+string[] animals = {"Cat", "Dog", "Bird", "Fish"};
+List<string> animalList = new(animals);
+animalList.ForEach(animal => Console.WriteLine(animal));
+
+// Clear
+Console.WriteLine("###########");
+animalList.Clear();
+animalList.ForEach(animal => Console.WriteLine(animal));
+
+List<Person> personList = new()
+{
+    new Person { Name = "John", Age = 25 },
+    new Person { Name = "Jane", Age = 30 },
+    new Person { Name = "Jack", Age = 35 }
+};
+
+foreach (Person person in personList)
+{
+    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
