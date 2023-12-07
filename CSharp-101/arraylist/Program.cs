b6ca7e9686faf619eba9008888c29e77aca7c873
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-12-08 01:10:39 +0300

    init: array list

---
+++ CSharp-101/arraylist/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+ArrayList list = new() { 1, 2, 3, 4, 5, };
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+// AddRange
+Console.WriteLine("**** AddRange ****");
+string[] strArr = { "emir", "halil", "mehmet" };
+list.AddRange(strArr);
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+// Add
+Console.WriteLine("**** Add ****");
+list.Add('a');
+list.Add(5.44);
+list.Add(false);
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+// Sort
+ArrayList intList = new() { 55, 44, 33, 22, 11 };
+
+foreach (var item in intList)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("**** Sort ****");
+
+intList.Sort();
+foreach (var item in intList)
+{
+    Console.WriteLine(item);
+}
+
+// Reverse
+Console.WriteLine("**** Reverse ****");
+intList.Reverse();
+foreach (var item in intList)
+{
+    Console.WriteLine(item);
+}
+
+// Clear
+Console.WriteLine("**** Clear ****");
+intList.Clear();
+foreach (var item in intList)
+{
+    Console.WriteLine(item);
+}
